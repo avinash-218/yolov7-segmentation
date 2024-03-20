0e9e402dd40a187f83dd48d4a57e63e90d68d3aa
avinash-218 <ravinash218@gmail.com>
2024-03-20 16:09:43 +0530

    output is same size as input

--- onnxruntime_inference.py
+++ onnxruntime_inference.py
@@ -2,6 +2,7 @@ import cv2, os
 import numpy as np
 import onnxruntime
 from util import *
+from pathlib import Path
 
 conf_thres = 0.25
 iou_thres = 0.45
@@ -14,31 +15,62 @@ model_path = "./"
 ONNX_MODEL = f"walls.onnx"
 video_path = "test.mp4"
 CLASSES = ['wall']
+classes = None
+agnostic_nms = False
+max_det = 1000
+save_crop = False
+save_img = True
+line_thickness = 3
+hide_labels = False
+hide_conf = False
+names = 'walls'
 
 sess = onnxruntime.InferenceSession(ONNX_MODEL)
 isExist = os.path.exists(result_path)
 if not isExist:
     os.makedirs(result_path)
 
-image_3c = cv2.imread(image_path)
-print('original shape', image_3c.shape)
-image_4c, image_3c = preprocess(image_3c, input_height, input_width)
-print('preprocessed shape', image_3c.shape, image_4c.shape)
+orig = cv2.imread(image_path)   #read image
+image_4c, image_3c = preprocess(orig, input_height, input_width)
 outputs = sess.run(['output', 'onnx::Slice_533', 'onnx::Slice_642', 'onnx::Slice_748', '517'],{"images": image_4c.astype(np.float32)}) # (1, 3, input height, input width)
-colorlist = gen_color(len(CLASSES))
-outputs = [outputs[0], outputs[4]]
-results = postprocess(outputs, image_4c, image_3c, conf_thres, iou_thres) ##[box,mask,shape]
-results = results[0]              ## batch=1
-boxes, masks, shape = results
-if isinstance(masks, np.ndarray):
-    mask_img, vis_img = vis_result(image_3c,  results, colorlist, CLASSES, result_path)
-    print('final images', mask_img.shape, vis_img.shape)
-    print('--> Save inference result')
-else:
-    print("No segmentation result")
 
+pred, proto = outputs[0], outputs[4]  #(1, 25200, 38) (1, 32, 160, 160)
 
-print("ONNX inference finish")
-cv2.destroyAllWindows()
+pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det, nm=32)
 
+for i, det in enumerate(pred):  # per image
+    p, im0 = image_path, orig.copy()
 
+    p = Path(p)  # to Path
+    save_path = str(f'._{i}')  # im.jpg
+    gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+    imc = im0.copy() if save_crop else im0  # for save_crop
+    annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+    if len(det):
+        masks = process_mask(proto[i], det[:, 6:], det[:, :4], image_4c.shape[2:], upsample=True)  # HWC
+
+        # Rescale boxes from img_size to im0 size
+        det[:, :4] = scale_coords(image_4c.shape[2:], det[:, :4], im0.shape).round()
+
+        # Mask plotting ----------------------------------------------------------------------------------------
+        mcolors = [colors(int(cls), True) for cls in det[:, 5]]
+        im_masks = plot_masks(image_4c[i], masks, mcolors)  # image with masks shape(imh,imw,3)
+        annotator.im = scale_masks(image_4c.shape[2:], im_masks, im0.shape)  # scale to original h, w
+        # Mask plotting ----------------------------------------------------------------------------------------
+
+        # Write results
+        for j, (*xyxy, conf, cls) in enumerate(reversed(det[:, :6])):
+            if save_img or save_crop:  # Add bbox to image
+                c = int(cls)  # integer class
+                label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                annotator.box_label(xyxy, label, color=colors(c, True))
+            if save_crop:
+                save_one_box(xyxy, imc, file='.' / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+
+    # Stream results
+    im0 = annotator.result()
+
+    # Save results (image with detections)
+    if save_img:
+        print('final image', im0.shape)
+        cv2.imwrite('result/'+image_path.split('/')[-1], im0)
--- util.py
+++ util.py
@@ -1,6 +1,83 @@
 import cv2
 import time
 import numpy as np
+from utils.plots import colors
+import torch
+from pathlib import Path
+import os
+import glob
+import math
+
+IMG_FORMATS = 'bmp', 'dng', 'jpeg', 'jpg', 'mpo', 'png', 'tif', 'tiff', 'webp'  # include image suffixes
+
+class LoadImages:
+    # YOLOv5 image/video dataloader, i.e. `python detect.py --source image.jpg/vid.mp4`
+    def __init__(self, path, img_size=640, stride=32, auto=True, transforms=None):
+        files = []
+        for p in sorted(path) if isinstance(path, (list, tuple)) else [path]:
+            p = str(Path(p).resolve())
+            if '*' in p:
+                files.extend(sorted(glob.glob(p, recursive=True)))  # glob
+            elif os.path.isdir(p):
+                files.extend(sorted(glob.glob(os.path.join(p, '*.*'))))  # dir
+            elif os.path.isfile(p):
+                files.append(p)  # files
+            else:
+                raise FileNotFoundError(f'{p} does not exist')
+
+        images = [x for x in files if x.split('.')[-1].lower() in IMG_FORMATS]
+        ni = len(images)
+
+        self.img_size = img_size
+        self.stride = stride
+        self.files = images
+        self.nf = ni
+        self.video_flag = [False] * ni
+        self.mode = 'image'
+        self.auto = auto
+        self.transforms = transforms  # optional
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        if self.count == self.nf:
+            raise StopIteration
+        path = self.files[self.count]
+
+        # Read image
+        self.count += 1
+        im0 = cv2.imread(path)  # BGR
+        assert im0 is not None, f'Image Not Found {path}'
+        s = f'image {self.count}/{self.nf} {path}: '
+
+        if self.transforms:
+            im = self.transforms(cv2.cvtColor(im0, cv2.COLOR_BGR2RGB))  # transforms
+        else:
+            im = letterbox(im0, self.img_size, stride=self.stride, auto=self.auto)[0]  # padded resize
+            im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+            im = np.ascontiguousarray(im)  # contiguous
+
+        return path, im, im0, s
+
+    def __len__(self):
+        return self.nf  # number of files
+
+def make_divisible(x, divisor):
+    # Returns nearest x divisible by divisor
+    if isinstance(divisor, torch.Tensor):
+        divisor = int(divisor.max())  # to int
+    return math.ceil(x / divisor) * divisor
+
+def check_img_size(imgsz, s=32, floor=0):
+    # Verify image size is a multiple of stride s in each dimension
+    if isinstance(imgsz, int):  # integer i.e. img_size=640
+        new_size = max(make_divisible(imgsz, int(s)), floor)
+    else:  # list i.e. img_size=[640, 480]
+        imgsz = list(imgsz)  # convert to list if tuple
+        new_size = [max(make_divisible(x, int(s)), floor) for x in imgsz]
+    return new_size
 
 def xywh2xyxy(x):
     # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
@@ -87,7 +164,6 @@ def nms(bboxes, scores, threshold=0.5):
 
     return keep
 
-
 def non_max_suppression(
         prediction,
         conf_thres=0.25,
@@ -173,7 +249,6 @@ def non_max_suppression(
 
     return output
 
-
 def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
     # Resize and pad image while meeting stride-multiple constraints
     shape = im.shape[:2]  # current shape [height, width]
@@ -227,6 +302,49 @@ def preprocess(image, input_height, input_width):
     
     return image_4c, image_3c
 
+def plot_masks(img, masks, colors, alpha=0.5):
+    print('img, mask', img.shape, masks.shape)  # (3, 640, 640) (35, 640, 640)
+    print('img, mask', type(img), type(masks))  #np np
+
+    img = torch.tensor(img)
+    masks = torch.tensor(masks)
+
+    """
+    Args:
+        img (tensor): img is in cuda, shape: [3, h, w], range: [0, 1]
+        masks (tensor): predicted masks on cuda, shape: [n, h, w]
+        colors (List[List[Int]]): colors for predicted masks, [[r, g, b] * n]
+    Return:
+        ndarray: img after draw masks, shape: [h, w, 3]
+
+    transform colors and send img_gpu to cpu for the most time.
+    """
+    img_gpu = img.clone()
+    num_masks = len(masks)
+    if num_masks == 0:
+        return img.permute(1, 2, 0).contiguous().cpu().numpy() * 255
+
+    # [n, 1, 1, 3]
+    # faster this way to transform colors
+    colors = torch.tensor(colors, device=img.device).float() / 255.0
+    colors = colors[:, None, None, :]
+    # [n, h, w, 1]
+    masks = masks[:, :, :, None]
+    masks_color = masks.repeat(1, 1, 1, 3) * colors * alpha
+    inv_alph_masks = masks * (-alpha) + 1
+    masks_color_summand = masks_color[0]
+    if num_masks > 1:
+        inv_alph_cumul = inv_alph_masks[:(num_masks - 1)].cumprod(dim=0)
+        masks_color_cumul = masks_color[1:] * inv_alph_cumul
+        masks_color_summand += masks_color_cumul.sum(dim=0)
+
+    # print(inv_alph_masks.prod(dim=0).shape) # [h, w, 1]
+    img_gpu = img_gpu.flip(dims=[0])  # filp channel for opencv
+    img_gpu = img_gpu.permute(1, 2, 0).contiguous()
+    # [h, w, 3]
+    img_gpu = img_gpu * inv_alph_masks.prod(dim=0) + masks_color_summand
+    return (img_gpu * 255).byte().cpu().numpy()
+
 def postprocess(preds, img, orig_img, conf_thres, iou_thres, classes=None):
     p = non_max_suppression(preds[0], conf_thres, iou_thres, classes)
     for i, pred in enumerate(p):  # per image
@@ -236,8 +354,9 @@ def postprocess(preds, img, orig_img, conf_thres, iou_thres, classes=None):
         if not len(preds):
             results.append([[], [], []])  # save empty boxes
             continue
-        masks = process_mask(proto[i], pred[:, 6:], pred[:, :4], img.shape[2:])  # HWC
+        masks = process_mask(proto[i], pred[:, 6:], pred[:, :4], img.shape[2:], upsample=True)  # HWC
         pred[:, :4] = scale_boxes(img.shape[2:], pred[:, :4], shape).round()
+
         results.append([pred[:, :6], masks, shape[:2]])
     return results
 
@@ -288,3 +407,203 @@ def vis_result(image_3c, results, colorlist, CLASSES, result_path):
     cv2.imwrite(f"./{result_path}/mask_image.jpg", mask_img)
     cv2.imwrite(f"./{result_path}/visual_image.jpg", vis_img)
     return mask_img, vis_img
+
+def clip_coords(boxes, shape):
+    # Clip bounding xyxy bounding boxes to image shape (height, width)
+    if isinstance(boxes, torch.Tensor):  # faster individually
+        boxes[:, 0].clamp_(0, shape[1])  # x1
+        boxes[:, 1].clamp_(0, shape[0])  # y1
+        boxes[:, 2].clamp_(0, shape[1])  # x2
+        boxes[:, 3].clamp_(0, shape[0])  # y2
+    else:  # np.array (faster grouped)
+        boxes[:, [0, 2]] = boxes[:, [0, 2]].clip(0, shape[1])  # x1, x2
+        boxes[:, [1, 3]] = boxes[:, [1, 3]].clip(0, shape[0])  # y1, y2
+
+def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
+    # Rescale coords (xyxy) from img1_shape to img0_shape
+    if ratio_pad is None:  # calculate from img0_shape
+        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
+
+    coords[:, [0, 2]] -= pad[0]  # x padding
+    coords[:, [1, 3]] -= pad[1]  # y padding
+    coords[:, :4] /= gain
+    clip_coords(coords, img0_shape)
+    return coords
+
+def scale_masks(img1_shape, masks, img0_shape, ratio_pad=None):
+    """
+    img1_shape: model input shape, [h, w]
+    img0_shape: origin pic shape, [h, w, 3]
+    masks: [h, w, num]
+    resize for the most time
+    """
+    # Rescale coords (xyxy) from img1_shape to img0_shape
+    if ratio_pad is None:  # calculate from img0_shape
+        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
+    tl_pad = int(pad[1]), int(pad[0])  # y, x
+    br_pad = int(img1_shape[0] - pad[1]), int(img1_shape[1] - pad[0])
+
+    if len(masks.shape) < 2:
+        raise ValueError(f'"len of masks shape" should be 2 or 3, but got {len(masks.shape)}')
+    # masks_h, masks_w, n
+    masks = masks[tl_pad[0]:br_pad[0], tl_pad[1]:br_pad[1]]
+    # 1, n, masks_h, masks_w
+    # masks = masks.permute(2, 0, 1).contiguous()[None, :]
+    # # shape = [1, n, masks_h, masks_w] after F.interpolate, so take first element
+    # masks = F.interpolate(masks, img0_shape[:2], mode='bilinear', align_corners=False)[0]
+    # masks = masks.permute(1, 2, 0).contiguous()
+    # masks_h, masks_w, n
+    masks = cv2.resize(masks, (img0_shape[1], img0_shape[0]))
+
+    # keepdim
+    if len(masks.shape) == 2:
+        masks = masks[:, :, None]
+
+    return masks
+
+def xyxy2xywh(x):
+    # Convert nx4 boxes from [x1, y1, x2, y2] to [x, y, w, h] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2  # x center
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2  # y center
+    y[:, 2] = x[:, 2] - x[:, 0]  # width
+    y[:, 3] = x[:, 3] - x[:, 1]  # height
+    return y
+
+def increment_path(path, exist_ok=False, sep='', mkdir=False):
+    # Increment file or directory path, i.e. runs/exp --> runs/exp{sep}2, runs/exp{sep}3, ... etc.
+    path = Path(path)  # os-agnostic
+    if path.exists() and not exist_ok:
+        path, suffix = (path.with_suffix(''), path.suffix) if path.is_file() else (path, '')
+
+        # Method 1
+        for n in range(2, 9999):
+            p = f'{path}{sep}{n}{suffix}'  # increment path
+            if not os.path.exists(p):  #
+                break
+        path = Path(p)
+
+    if mkdir:
+        path.mkdir(parents=True, exist_ok=True)  # make directory
+
+    return path
+
+def save_one_box(xyxy, im, file=Path('im.jpg'), gain=1.02, pad=10, square=False, BGR=False, save=True):
+    # Save image crop as {file} with crop size multiple {gain} and {pad} pixels. Save and/or return crop
+    xyxy = torch.tensor(xyxy).view(-1, 4)
+    b = xyxy2xywh(xyxy)  # boxes
+    if square:
+        b[:, 2:] = b[:, 2:].max(1)[0].unsqueeze(1)  # attempt rectangle to square
+    b[:, 2:] = b[:, 2:] * gain + pad  # box wh * gain + pad
+    xyxy = xywh2xyxy(b).long()
+    clip_coords(xyxy, im.shape)
+    crop = im[int(xyxy[0, 1]):int(xyxy[0, 3]), int(xyxy[0, 0]):int(xyxy[0, 2]), ::(1 if BGR else -1)]
+    if save:
+        file.parent.mkdir(parents=True, exist_ok=True)  # make directory
+        f = str(increment_path(file).with_suffix('.jpg'))
+        # cv2.imwrite(f, crop)  # save BGR, https://github.com/ultralytics/yolov5/issues/7007 chroma subsampling issue
+        Image.fromarray(crop[..., ::-1]).save(f, quality=95, subsampling=0)  # save RGB
+    return crop
+
+from PIL import Image, ImageDraw, ImageFont
+
+def is_ascii(s=''):
+    # Is string composed of all ASCII (no UTF) characters? (note str().isascii() introduced in python 3.7)
+    s = str(s)  # convert list, tuple, None, etc. to str
+    return len(s.encode().decode('ascii', 'ignore')) == len(s)
+
+class Annotator:
+    # YOLOv5 Annotator for train/val mosaics and jpgs and detect/hub inference annotations
+    def __init__(self, im, line_width=None, font_size=None, font='Arial.ttf', pil=False, example='abc'):
+        assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to Annotator() input images.'
+        non_ascii = not is_ascii(example)  # non-latin labels, i.e. asian, arabic, cyrillic
+        self.pil = pil or non_ascii
+        if self.pil:  # use PIL
+            self.im = im if isinstance(im, Image.Image) else Image.fromarray(im)
+            self.draw = ImageDraw.Draw(self.im)
+            self.font = ImageFont.load_default()
+        else:  # use cv2
+            self.im = im
+        self.lw = line_width or max(round(sum(im.shape) / 2 * 0.003), 2)  # line width
+
+    def box_label(self, box, label='', color=(128, 128, 128), txt_color=(255, 255, 255)):
+        # Add one xyxy box to image with label
+        if self.pil or not is_ascii(label):
+            self.draw.rectangle(box, width=self.lw, outline=color)  # box
+            if label:
+                w, h = self.font.getsize(label)  # text width, height
+                outside = box[1] - h >= 0  # label fits outside box
+                self.draw.rectangle(
+                    (box[0], box[1] - h if outside else box[1], box[0] + w + 1,
+                     box[1] + 1 if outside else box[1] + h + 1),
+                    fill=color,
+                )
+                # self.draw.text((box[0], box[1]), label, fill=txt_color, font=self.font, anchor='ls')  # for PIL>8.0
+                self.draw.text((box[0], box[1] - h if outside else box[1]), label, fill=txt_color, font=self.font)
+        else:  # cv2
+            p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
+            cv2.rectangle(self.im, p1, p2, color, thickness=self.lw, lineType=cv2.LINE_AA)
+            if label:
+                tf = max(self.lw - 1, 1)  # font thickness
+                w, h = cv2.getTextSize(label, 0, fontScale=self.lw / 3, thickness=tf)[0]  # text width, height
+                outside = p1[1] - h >= 3
+                p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
+                cv2.rectangle(self.im, p1, p2, color, -1, cv2.LINE_AA)  # filled
+                cv2.putText(self.im,
+                            label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2),
+                            0,
+                            self.lw / 3,
+                            txt_color,
+                            thickness=tf,
+                            lineType=cv2.LINE_AA)
+
+    def draw_trk(self,thickness,centroids):
+        [cv2.line(self.im, (int(centroids.centroids[i][0]),int(centroids.centroids[i][1])),
+                (int(centroids.centroids[i+1][0]),int(centroids.centroids[i+1][1])),
+                (255,144,30), thickness=thickness) for i,_ in  enumerate(centroids.centroids)
+                if i < len(centroids.centroids)-1 ]
+
+    def draw_id(self, bbox, identities=None, categories=None, names=None, offset=(0, 0)):
+        for i, box in enumerate(bbox):
+            x1, y1, x2, y2 = [int(i) for i in box]
+            x1 += offset[0]
+            x2 += offset[0]
+            y1 += offset[1]
+            y2 += offset[1]
+            cat = int(categories[i]) if categories is not None else 0
+            id = int(identities[i]) if identities is not None else 0
+            data = (int((box[0]+box[2])/2),(int((box[1]+box[3])/2)))
+            label = str(id)
+            (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+            cv2.rectangle(self.im, (x1, y1 - 20), (x1 + w, y1), (255,144,30), -1)
+            cv2.putText(self.im, label, (x1, y1 - 5),cv2.FONT_HERSHEY_SIMPLEX, 0.6, [255, 255, 255], 1)
+            cv2.circle(self.im, data, 4, (255,0,255),-1)
+
+    def rectangle(self, xy, fill=None, outline=None, width=1):
+        # Add rectangle to image (PIL-only)
+        self.draw.rectangle(xy, fill, outline, width)
+
+    def text(self, xy, text, txt_color=(255, 255, 255), anchor='top'):
+        # Add text to image (PIL-only)
+        if anchor == 'bottom':  # start y from font bottom
+            w, h = self.font.getsize(text)  # text width, height
+            xy[1] += 1 - h
+        self.draw.text(xy, text, fill=txt_color, font=self.font)
+
+    def fromarray(self, im):
+        # Update self.im from a numpy array
+        self.im = im if isinstance(im, Image.Image) else Image.fromarray(im)
+        self.draw = ImageDraw.Draw(self.im)
+
+    def result(self):
+        # Return annotated image as array
+        return np.asarray(self.im)
+
